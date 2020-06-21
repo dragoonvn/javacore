928a863f7754592394fb4498ffbd25c3dd12556c
dragoonvn <trietvm@hotmail.com>
2020-06-21 15:25:24 +0700

    bai

--- src/activity34/Activity34.java
+++ src/activity34/Activity34.java
@@ -1,4 +1,27 @@
 package activity34;
 
 public class Activity34 {
+    public static void main(String[] args) {
+        System.out.println("Các số chía hết cho 3: ");
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+        System.out.println("Các số chía hết cho 5: ");
+        for (int i = 1; i <= 100; i++) {
+            if (i % 5 == 0) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+        System.out.println("Các số chía hết cho 3 và 5: ");
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.print(i + " ");
+            }
+        }
+
+    }
 }
--- src/activity41/Acitivity41.java
+++ src/activity41/Acitivity41.java
@@ -1,4 +1,20 @@
 package activity41;
 
 public class Acitivity41 {
+    public static void main(String[] args) {
+        int i, a, b, tc = 0;
+        System.out.println("Print out all Armstrong: ");
+        for (i = 1; i <= 500; i++) {
+            a = i;
+            while (a > 0) {
+                b = a % 10;
+                tc = tc + (b * b * b);
+                a = a / 10;
+            }
+            if (tc == i) {
+                System.out.print(i + ", ");
+            }
+            tc = 0;
+        }
+    }
 }
