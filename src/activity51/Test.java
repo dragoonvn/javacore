39bec90f75f0c3ddcdd0875be16f485f0bb9946a
dragoonvn <trietvm@hotmail.com>
2020-07-02 23:04:10 +0700

    Activity 46

--- src/activity51/Test.java
+++ src/activity51/Test.java
@@ -4,12 +4,14 @@ import java.util.Scanner;
 
 public class Test {
     public static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
         float width, height;
         System.out.println("Nhập vào width: ");
         width = scanner.nextFloat();
         System.out.println("Nhập vào height: ");
         height = scanner.nextFloat();
+        scanner.close();
         RectangleOOp rectangleOOp = new RectangleOOp(width, height);
         System.out.println("Chu vi hình: " + rectangleOOp.tinhChuVi());
         System.out.println("Diện tích hình: " + rectangleOOp.tinhDienTich());
